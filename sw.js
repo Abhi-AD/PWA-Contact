a6ea523d6c66d49742d371134cca8013424d9832
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-01-05 11:27:28 +0545

    service worker install

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('service worker registered sw.js');
+// console.log('service worker registered contact');
+
+
+// install ServiceWorker
+self.addEventListener('install', evt=>{
+     console.log('service worker has been installed.')
+})
